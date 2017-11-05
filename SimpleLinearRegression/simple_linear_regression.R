4a9a106abceb1dce6051a7d89c364ac43a399736
Vikasjce <vikas.jce@gmail.com>
2017-11-05 11:32:27 +0530

    message

---
+++ SimpleLinearRegression/simple_linear_regression.R
@@ -0,0 +1,49 @@
+# Simple Linear Regression
+
+# Import the dataset 
+dataset <- read.csv("Salary_Data.csv")
+
+# Fixing null values
+
+
+# Encoding the Categorical Data
+
+
+# Split the data into Train and the test set 
+library(caTools)
+set.seed(123)
+split <- sample.split(dataset$Salary,SplitRatio = 2/3)
+training_set <- subset(dataset,split==TRUE)
+test_set <- subset(dataset,split==FALSE)
+
+# Feature Scaling 
+
+
+# Build the Linear Regression Model
+regressor <- lm(formula = Salary~ YearsExperience,data = training_set)
+summary(regressor)
+
+# Predicting the test set results 
+y_pred <- predict(regressor,newdata = test_set)
+
+# Visualizing the Training set results 
+ggplot()+
+  geom_point(aes(x=training_set$YearsExperience,y=training_set$Salary),
+             colour='red')+
+  geom_line(aes(x=training_set$YearsExperience,y=predict(regressor,newdata = training_set)),
+            colour="blue")+
+  ggtitle("Salary vs Experience(Training Set)")+
+  xlab("Years Of Experience")+
+  ylab("Salary")
+
+# Visualizing the Test set results 
+ggplot()+
+  geom_point(aes(x=test_set$YearsExperience,y=test_set$Salary),
+             colour='red')+
+  geom_line(aes(x=training_set$YearsExperience,y=predict(regressor,newdata = training_set)),
+            colour="blue")+
+  ggtitle("Salary vs Experience(Test Set)")+
+  xlab("Years Of Experience")+
+  ylab("Salary")
+  
+
